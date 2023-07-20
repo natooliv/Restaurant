8e7642eaa40028c6d2149aee0f12b70c1d75cf06
Natalia de Oliveira <natoliveira7300hotmail.com>
2023-07-20 16:07:37 -0300

    Req 03

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,26 @@
+from src.models.dish import Dish, Ingredient
+import csv
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+
+        with open(source_path) as restaurant_file_csv:
+            data_reader = csv.DictReader(restaurant_file_csv)
+            dish_map = {}
+
+            for row in data_reader:
+                dish_name = row["dish"]
+                recipe_price = float(row["price"])
+                ingredient_name = row["ingredient"]
+                ingredient_amount = int(row["recipe_amount"])
+
+                if dish_name not in dish_map:
+                    dish_map[dish_name] = Dish(dish_name, recipe_price)
+
+                dish_map[dish_name].add_ingredient_dependency(
+                    Ingredient(ingredient_name), ingredient_amount)
+
+            self.dishes = set(dish_map.values())
