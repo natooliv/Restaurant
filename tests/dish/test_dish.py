128d066af76402c8ed5694605799170915183110
Natalia de Oliveira <natoliveira7300hotmail.com>
2023-07-20 15:34:57 -0300

    Req 02

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,26 @@
-from src.models.dish import Dish  # noqa: F401, E261, E501
+import pytest
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient, Restriction
 
 
-# Req 2
 def test_dish():
-    pass
+    bacon_burguer_dish = Dish('Lombo Canadense', 30.0)
+    assert bacon_burguer_dish.name == 'Lombo Canadense'
+    assert bacon_burguer_dish.price == 30.0
+    assert bacon_burguer_dish == Dish('Lombo Canadense', 30.0)
+    assert repr(bacon_burguer_dish) == "Dish('Lombo Canadense', R$30.00)"
+    assert hash(bacon_burguer_dish) == hash(Dish('Lombo Canadense', 30.0))
+    assert hash(bacon_burguer_dish) != hash(Dish('Pizza', 28.0))
+
+    with pytest.raises(TypeError):
+        Dish('Lombo Canadense', '-30')
+    with pytest.raises(ValueError):
+        Dish('Lombo Canadense', -30)
+
+    bacon_ingredient = Ingredient('bacon')
+    bacon_burguer_dish.add_ingredient_dependency(bacon_ingredient, 1)
+    assert bacon_burguer_dish.recipe == {Ingredient('bacon'): 1}
+    assert bacon_burguer_dish.get_ingredients() == {bacon_ingredient}
+    assert bacon_burguer_dish.get_restrictions() == {
+        Restriction.ANIMAL_MEAT, Restriction.ANIMAL_DERIVED
+        } 
