e97a21cc619f8812b8ae59313b76135202b7bcc0
Natalia de Oliveira <natoliveira7300hotmail.com>
2023-07-20 15:28:13 -0300

    Rq 01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,35 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
 # Req 1
 def test_ingredient():
-    pass
+    # Teste 'queijo mussarela'
+    mozzarella = Ingredient("queijo mussarela")
+    assert mozzarella.name == "queijo mussarela"
+    assert mozzarella.restrictions == {
+        Restriction.LACTOSE, Restriction.ANIMAL_DERIVED
+    }
+    assert mozzarella == Ingredient("queijo mussarela")
+    assert mozzarella != Ingredient("ovo")
+    assert hash(mozzarella) == hash("queijo mussarela")
+    assert hash(mozzarella) != hash(Ingredient("ovo"))
+    assert repr(mozzarella) == "Ingredient('queijo mussarela')"
+
+    # Teste 'farinha'
+    farinha = Ingredient("farinha")
+    assert farinha.name == "farinha"
+    assert farinha.restrictions == {Restriction.GLUTEN}
+    assert farinha == Ingredient("farinha")
+    assert farinha != Ingredient("ovo")
+    assert hash(farinha) == hash("farinha")
+    assert hash(farinha) != hash(Ingredient("ovo"))
+    assert repr(farinha) == "Ingredient('farinha')"
+
+    # Teste  'sal'
+    sal = Ingredient("sal")
+    assert sal.name == "sal"
+    assert sal.restrictions == set()
+    assert sal == Ingredient("sal")
+    assert sal != Ingredient("ovo")
+    assert hash(sal) == hash("sal")
+    assert repr(sal) == "Ingredient('sal')"
